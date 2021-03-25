cc86813187601f95aca9b3d5d484d785df61d808
jiyoung <jiyoung4061@naver.com>
2021-03-25 14:28:09 +0900

    influx sample

--- mysite05/src/main/java/com/markany/config/app/InfluxDBConfiguration.java
+++ mysite05/src/main/java/com/markany/config/app/InfluxDBConfiguration.java
@@ -1,23 +1,47 @@
 package com.markany.config.app;
 
+import org.apache.ibatis.mapping.Environment;
+import org.influxdb.InfluxDBFactory;
 import org.influxdb.dto.Point;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.influxdb.DefaultInfluxDBTemplate;
 import org.springframework.data.influxdb.InfluxDBConnectionFactory;
 import org.springframework.data.influxdb.InfluxDBProperties;
 import org.springframework.data.influxdb.InfluxDBTemplate;
 import org.springframework.data.influxdb.converter.PointConverter;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @EnableConfigurationProperties(InfluxDBProperties.class)
+@PropertySource("classpath:com/markany/mysite/config/app/properties/jdbc.properties")
 public class InfluxDBConfiguration {
+	
+	@Autowired
+	private Environment env;
+
+//	@Bean
+//	public DataSource datasource() {
+//		BasicDataSource dataSource = new BasicDataSource();
+//		dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+//		dataSource.setUrl(env.getProperty("jdbc.url"));
+//		dataSource.setUsername(env.getProperty("jdbc.username"));
+//		dataSource.setPassword(env.getProperty("jdbc.password"));
+//		dataSource.setInitialSize(env.getProperty("jdbc.initialSize", Integer.class)); // getProperty반환값이 string이라 int값으로 바꿔줌!
+//		dataSource.setMaxActive(env.getProperty("jdbc.maxActive", Integer.class));
+//		
+//		return dataSource;
+//	}
 	
 	@Bean
 	  public InfluxDBConnectionFactory connectionFactory(final InfluxDBProperties properties)
 	  {
-	    return new InfluxDBConnectionFactory(properties);
+		InfluxDBConnectionFactory influxDBConnectionFactory = new InfluxDBConnectionFactory(properties);
+		return influxDBConnectionFactory;
 	  }
 
 	  @Bean
@@ -40,22 +64,8 @@ public class InfluxDBConfiguration {
 	    return new DefaultInfluxDBTemplate(connectionFactory);
 	  }
 	
-//	@Autowired
-//	private Environment env;
-//	
-//	@Bean
-//	public DataSource datasource() {
-//		BasicDataSource dataSource = new BasicDataSource();
-//		dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
-//		dataSource.setUrl(env.getProperty("jdbc.url"));
-//		dataSource.setUsername(env.getProperty("jdbc.username"));
-//		dataSource.setPassword(env.getProperty("jdbc.password"));
-//		dataSource.setInitialSize(env.getProperty("jdbc.initialSize", Integer.class)); // getProperty반환값이 string이라 int값으로 바꿔줌!
-//		dataSource.setMaxActive(env.getProperty("jdbc.maxActive", Integer.class));
-//		
-//		return dataSource;
-//	}
-//	
+	
+	
 //	@Bean
 //	public PlatformTransactionManager transactionManager(DataSource dataSource) {
 //		return new DataSourceTransactionManager(dataSource);
